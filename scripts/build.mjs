750a2b76ec0cd4607bc38c9b6627776c52d2517a
Eric Richardson <eric@decompiled.dev>
2026-07-18 10:58:18 -0400

    Build Kindness Blaster arcade game

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -2,9 +2,11 @@ import { cp, mkdir, rm } from "node:fs/promises";
 
 const outputDirectory = new URL("../dist/", import.meta.url);
 const sourceFile = new URL("../index.html", import.meta.url);
+const domainFile = new URL("../CNAME", import.meta.url);
 
 await rm(outputDirectory, { recursive: true, force: true });
 await mkdir(outputDirectory, { recursive: true });
 await cp(sourceFile, new URL("index.html", outputDirectory));
+await cp(domainFile, new URL("CNAME", outputDirectory));
 
 console.log("Built site in dist/");
